24f06863f0911e18e107f8d6d57424baad2fec77
Rehammashal7 <rehammashal7@gmail.com>
2023-02-13 14:00:49 +0200

    create enum class

--- src/main/kotlin/interactor/GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor.kt
@@ -7,40 +7,40 @@ class GetCityHasHighestDifferentRentBetweenCityCenterAndOutsideInteractor(
     private val dataSource: CostOfLivingDataSource
 ) {
 
-    fun execute(type: Int): String? {
-        if (type == 1) {
-            return dataSource.getAllCitiesData()
-                .filter(::excludeNullRentOneTypeOneAndLowQualityData)
-                .maxByOrNull { abs(it.realEstatesPrices.apartmentOneBedroomInCityCentre!!
-                        - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!) }!!
-                .cityName
-        } else if (type == 2) {
-            return  return dataSource.getAllCitiesData()
-                .filter(::excludeNullRentOneTypeTwoAndLowQualityData)
-                .maxByOrNull { abs(it.realEstatesPrices.apartment3BedroomsInCityCentre!! -
-                        it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!) }!!
-                .cityName
-        }
-        else return null
+    fun execute(type:TheTypeOfApartments): CityEntity {
+//        if (type == 1) {
+//            return dataSource.getAllCitiesData()
+//                .filter(::excludeNullRentOneTypeOneAndLowQualityData)
+//                .maxByOrNull { abs(it.realEstatesPrices.apartmentOneBedroomInCityCentre!!
+//                        - it.realEstatesPrices.apartmentOneBedroomOutsideOfCentre!!) }!!
+//                .cityName
+//        } else if (type == 2) {
+//            return  return dataSource.getAllCitiesData()
+//                .filter(::excludeNullRentOneTypeTwoAndLowQualityData)
+//                .maxByOrNull { abs(it.realEstatesPrices.apartment3BedroomsInCityCentre!! -
+//                        it.realEstatesPrices.apartment3BedroomsOutsideOfCentre!!) }!!
+//                .cityName
+//        }
+//        else return null
+return dataSource.getAllCitiesData()[0]
+    }}
 
-    }
-
-    private fun excludeNullRentOneTypeOneAndLowQualityData(city: CityEntity): Boolean {
-        return city.dataQuality
-                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
-                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
-    }
-
-    private fun excludeNullRentOneTypeTwoAndLowQualityData(city: CityEntity): Boolean {
-
-        return city.dataQuality
-                && city.realEstatesPrices.apartment3BedroomsInCityCentre != null
-                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
-
-
-    }
-
-}
+//    private fun excludeNullRentOneTypeOneAndLowQualityData(city: CityEntity): Boolean {
+//        return city.dataQuality
+//                && city.realEstatesPrices.apartmentOneBedroomInCityCentre != null
+//                && city.realEstatesPrices.apartmentOneBedroomOutsideOfCentre != null
+//    }
+//
+//    private fun excludeNullRentOneTypeTwoAndLowQualityData(city: CityEntity): Boolean {
+//
+//        return city.dataQuality
+//                && city.realEstatesPrices.apartment3BedroomsInCityCentre != null
+//                && city.realEstatesPrices.apartment3BedroomsOutsideOfCentre != null
+//
+//
+//    }
+//
+//}
 
 
 
---
+++ src/main/kotlin/interactor/TheTypeOfApartments.kt
@@ -0,0 +1,6 @@
+package interactor
+
+enum class TheTypeOfApartments {
+    ONE_BED_ROOM,
+    THREE_BED_ROOMS
+}
