a571f05804111948418332073ab6bfbd2b82d258
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-02-18 09:39:23 +0200

    add GetCityHasMostSuitableFitnessClubInteractor class

---
+++ src/main/kotlin/interactor/GetCityHasMostSuitableFitnessClubInteractor.kt
@@ -0,0 +1,8 @@
+package interactor
+
+class GetCityHasMostSuitableFitnessClubInteractor(private val dataSource: CostOfLivingDataSource) {
+
+    fun execute(): List<Triple<String, Float, Float>> {
+        TODO("NOT IMPLEMENTED YET")
+    }
+}
