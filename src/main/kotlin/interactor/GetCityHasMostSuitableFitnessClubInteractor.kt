24e1857befc307df7f13f814ed8ce72510da8266
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-02-18 10:30:30 +0200

    add excludeLowQualityData function

--- src/main/kotlin/interactor/GetCityHasMostSuitableFitnessClubInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasMostSuitableFitnessClubInteractor.kt
@@ -1,8 +1,14 @@
 package interactor
 
+import model.CityEntity
+
 class GetCityHasMostSuitableFitnessClubInteractor(private val dataSource: CostOfLivingDataSource) {
 
     fun execute(): List<Triple<String, Float, Float>> {
+        TODO("NOT IMPLEMENTED YET")
+    }
+
+    fun excludeLowQualityData(city: CityEntity): Boolean {
         TODO("NOT IMPLEMENTED YET")
     }
 }
