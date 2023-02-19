4af1e252b8bf25c1eac2f579c49cac585846aeef
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-02-19 03:30:28 +0200

    implement class GetCityHasCheapestCappuccinoRegularInRestaurantsInteractor

---
+++ src/main/kotlin/interactor/GetCityHasCheapestCappuccinoRegularInRestaurantsInteractor.kt
@@ -0,0 +1,25 @@
+package interactor
+
+import model.CityEntity
+
+/**
+ *  Created by Rehab Fakhry on Sat, 2/18/2023 at 11:46 PM.
+ */
+
+class GetCityHasCheapestCappuccinoRegularInRestaurantsInteractor (
+    private val dataSource: CostOfLivingDataSource
+) {
+    fun execute(countryName: String): List<Pair<String, Float>> {
+        return dataSource
+            .getAllCitiesData()
+            .filter { excludeNullCappuccinoRegularInRestaurantsAndLowQualityData(it) &&
+                    it.country == countryName }
+            .sortedBy { it.drinksPrices.cappuccinoRegularInRestaurants }
+            .map { Pair(it.cityName, it.drinksPrices.cappuccinoRegularInRestaurants!!)}
+            .takeIf { it.isNotEmpty() } ?: throw Exception("No cities found in country $countryName")
+    }
+
+    fun excludeNullCappuccinoRegularInRestaurantsAndLowQualityData(city: CityEntity): Boolean {
+        return city.drinksPrices.cappuccinoRegularInRestaurants != null && city.dataQuality
+    }
+}
